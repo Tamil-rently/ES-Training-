a434e2e466a79f5ec88af6bbd9264ac5a50d6ed4
Tamil-rently <tamilarasan.r@rently.com>
2021-12-13 21:39:05 +0530

    Cmake and maketools build

---
+++ Assignment 1/build/CMakeFiles/linkedlist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkedlist.dir/linked_list_implementation.c.o"
+  "linkedlist"
+  "linkedlist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linkedlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
